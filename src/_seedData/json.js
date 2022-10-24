599dedbf1751e176efb8c160c27887256fd24035
cosm1co <federicofaraz@gmail.com>
2022-10-24 17:41:14 -0300

    first commit

---
+++ src/_seedData/json.js
@@ -0,0 +1,82 @@
+export const bovinejson = [
+    {
+        "SENASA_ID": "AGUADA098765432",
+        "type": "TORO",
+        "weight": "347",
+        "potrero": "VACA MUERTA",
+        "device": "CARAVANA",
+        "n_device": "CARAV198"
+      },
+      {
+        "SENASA_ID": "AGUADA098765443",
+        "type": "VAQUILLONA",
+        "weight": "320",
+        "potrero": "VACA FELIZ",
+        "device": "COLLAR",
+        "n_device": "COLLAR69"
+      },
+      {
+        "SENASA_ID": "CORRAL0122966789",
+        "type": "NOVILLO",
+        "weight": "230",
+        "potrero": "vaquitas locas",
+        "device": "CARAVANA",
+        "n_device": "CARAV123"
+      },
+      {
+        "SENASA_ID": "AGUADA098765756",
+        "type": "TORO",
+        "weight": "400",
+        "potrero": "VACA LOCA",
+        "device": "CARAVANA",
+        "n_device": "907AVANA"
+      },
+      {
+        "SENASA_ID": "CORRAL0123402189",
+        "type": "VAQUILLONA",
+        "weight": "350",
+        "potrero": "VACA MUERTA",
+        "device": "COLLAR",
+        "n_device": "COLLAR56"
+      },
+      {
+        "SENASA_ID": "AGUADA098048432",
+        "type": "TORO",
+        "weight": "200",
+        "potrero": "VACA LOCA",
+        "device": "CARAVANA",
+        "n_device": "908AVANA"
+      },
+      {
+        "SENASA_ID": "CORRAL0123375789",
+        "type": "NOVILLO",
+        "weight": "250",
+        "potrero": "VACA MUERTA",
+        "device": "CARAVANA",
+        "n_device": "CA345ANA"
+      },
+      {
+        "SENASA_ID": "AGUADA542765432",
+        "type": "TORO",
+        "weight": "400",
+        "potrero": "VACA FELIZ",
+        "device": "COLLAR",
+        "n_device": "COLLAR28"
+      },
+      {
+        "SENASA_ID": "CORRAL0123455559",
+        "type": "VAQUILLONA",
+        "weight": "300",
+        "potrero": "VACA FELIZ",
+        "device": "COLLAR",
+        "n_device": "COLLAR54"
+      },
+      {
+        "SENASA_ID": "AGUADA098765489",
+        "type": "TORO",
+        "weight": "347",
+        "potrero": "VACA MUERTA",
+        "device": "CARAVANA",
+        "n_device": "CARAV108"
+      } 
+    ]
